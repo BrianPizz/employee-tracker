4aa8858257b2e3fce881c7d19ee6ce8991372a26
BrianPizz <bpizzimentijr@outlook.com>
2023-10-11 21:42:31 -0700

    view all functions and exit prompt

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = require('express');
 // Import and require mysql2 and inquirer
-const mysql = require('mysql12');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
 // Connect to database
 const db = mysql.createConnection(
@@ -15,9 +14,64 @@ const db = mysql.createConnection(
     console.log(`Connected to the books_db database.`)
 );
 
-function init() { 
+const prompt = () => {
     inquirer
-        .prompt()
-
-        .then()
+        .prompt([{
+            type: 'list',
+            name: 'options',
+            message: 'What would oyu like to do?',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Exit']
+        }])
+        .then((answers) => {
+            switch (answers.options) {
+                case 'View All Employees':
+                    return viewEmployees();
+                case 'Add Employee':
+                // function 
+                case 'Update Employee Role':
+                // function 
+                case 'View All Roles':
+                    return viewRoles();
+                case 'Add Role':
+                // function 
+                case 'View All Departments':
+                    return viewDepartments();
+                case 'Add Department':
+                // function 
+                case 'Exit':
+                    return process.exit()
+            }
+        })
 }
+// View all employees
+const viewEmployees = () => {
+    db.query(`SELECT e.id, e.first_name, e.last_name, r.title, d.department_name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee AS e
+    JOIN role AS r ON e.role_id = r.id 
+    JOIN department AS d ON r.department_id = d.id
+    LEFT JOIN employee AS m ON e.manager_id = m.id;`, function (err, results) {
+        if (err) throw err;
+        console.log(results);
+        prompt();
+    })
+}
+
+// view all roles with id title and salary
+const viewRoles = () => {
+    db.query('SELECT role.id, role.title, role.salary, department.department_name AS department FROM role JOIN department ON role.department_id = department.id ORDER BY role.id;', function (err, results) {
+        if (err) throw err;
+        console.log(results);
+        prompt();
+    })
+}
+
+// view all departments
+const viewDepartments = () => {
+    db.query('SELECT * FROM department', function (err, results) {
+        if (err) throw err;
+        console.log(results);
+        prompt();
+    })
+}
+
+prompt();
